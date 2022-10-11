aa09f8e98da3a8c12b3d606a4778d990291d5a01
Niha Siddiqui <niha.siddiqui@obcc.de>
2022-10-11 11:09:46 +0200

    company update

--- app/Http/Controllers/API/CompanyAPIController.php
+++ app/Http/Controllers/API/CompanyAPIController.php
@@ -2,6 +2,7 @@
 /* *created by Niha Siddiqui 2022-09-02
     * Company registration controller methods
 */
+
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\API\BaseController as BaseController;
@@ -18,16 +19,30 @@ class CompanyAPIController extends BaseController
     //
     public function index(Request $request)
     {
-        // \DB::enableQueryLog(); // Enable query log
-        
-        // get all Companies list with company, duties, positions and certificates
+
         $data = Company::leftJoin('employees', 'employees.company_id', '=', 'companies.id')
-        ->select('companies.id', 'company_type_id', 'company_name', 'company_department', 'company_started_at', 'company_ended_at',\DB::raw('COUNT(employees.id) as employees'))
-        ->groupBy('companies.id')->whereNotNull('status')
-            ->orderBy('companies.created_at', 'desc')
-            ->paginate($request->limit);
-            // dd(\DB::getQueryLog()); // Show results of log
-        return response()->json(['data'=> $data], 200);
+            ->select('companies.id', 'company_type_id', 'company_name', 'company_department', 'company_started_at', 'company_ended_at', \DB::raw('COUNT(employees.id) as employees'))
+            ->groupBy('companies.id')->whereNotNull('status')
+            ->orderBy('companies.created_at', 'desc');
+
+        if (!Auth()->user()->hasRole('super-admin')) {
+
+            if (!empty(Auth()->user()->company_id) && Auth()->user()->company_id != null) {
+                $authCompanyId = Auth()->user()->company_id;
+            }
+            $data = $data->where('companies.id', $authCompanyId);
+        }
+        $data = $data->paginate($request->limit);
+        // \DB::enableQueryLog(); // Enable query log
+
+        // get all Companies list with company, duties, positions and certificates
+        // $data = Company::leftJoin('employees', 'employees.company_id', '=', 'companies.id')
+        //     ->select('companies.id', 'company_type_id', 'company_name', 'company_department', 'company_started_at', 'company_ended_at', \DB::raw('COUNT(employees.id) as employees'))
+        //     ->groupBy('companies.id')->whereNotNull('status')
+        //     ->orderBy('companies.created_at', 'desc')
+        //     ->paginate($request->limit);
+        // dd(\DB::getQueryLog()); // Show results of log
+        return response()->json(['data' => $data], 200);
     }
 
     public function store(request $request)
@@ -40,60 +55,64 @@ class CompanyAPIController extends BaseController
             'company_started_at' => 'date|date_format:Y-m-d|required',
             'company_ended_at' => 'date|date_format:Y-m-d|nullable'
         ]);
-   
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $input = $request->all();
+        try {
+            $result = Company::create($input);
+            if ($result) {
+                return response()->json(['message' => 'company added succesfully.', 'data' => $result], 200);
+            }
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    public function show($id)
+    {
+        $data = [];
+        if (Auth()->user()->hasRole('super-admin') || Auth()->user()->hasRole('pro-admin') || Auth()->user()->hasRole('standard-admin')) {
+            $data = Company::with('employees')->find($id);
+            return response()->json([
+                'data' => $data
+            ], 200);
+        } else {
+            return ResponseHandler::validationError(['The user has no permission to view details.']);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        // save employee w.r.t ompany, duties, positions and certificates
+        $validator = Validator::make($request->all(), [
+            'company_id' => 'required',
+            'company_type_id' => 'required',
+            'company_name' => 'required',
+            'company_department' => 'required',
+            'company_started_at' => 'date|date_format:Y-m-d|required',
+            'company_ended_at' => 'date|date_format:Y-m-d|nullable'
+        ]);
+
+
+
         if($validator->fails()){
             return $this->sendError('Validation Error.', $validator->errors());       
         }
 
         $input = $request->all();
         try{
-            $result = Company::create($input);
+            $result = Company::whereId($id)->update(array_except($input, ['company_id']));
             if($result) {
-                return response()->json(['message' => 'company added succesfully.', 'data' => $result], 200);
+                return response()->json(['message' => 'company updated succesfully.', 'data' => $result], 200);
             }
-            
-        } catch(Exception $e) {
-            dd($e->getMessage());
+
+        } catch(\Exception $e) {
+            return ResponseHandler::serverError($e);
         }
-        
-
-    }
-
-    public function show($id)
-    {
-        $data = Company::with('employees')->find($id);
-        return response()->json([
-            'data' => $data
-        ], 200);
-
-    }
-
-    public function update(Request $request, $id)
-    {
-        // save employee w.r.t ompany, duties, positions and certificates
-        // $validator = Validator::make($request->all(), [
-        //     'company_type_id' => 'required',
-        //     'company_name' => 'required',
-        //     'company_department' => 'required',
-        //     'company_started_at' => 'date|date_format:Y-m-d|required',
-        //     'company_ended_at' => 'date|date_format:Y-m-d|nullable'
-        // ]);
-   
-        // if($validator->fails()){
-        //     return $this->sendError('Validation Error.', $validator->errors());       
-        // }
-
-        $input = $request->all();
-        dd($input);
-        // try{
-        //     $result = Company::create($input);
-        //     if($result) {
-        //         return response()->json(['message' => 'company added succesfully.', 'data' => $result], 200);
-        //     }
-            
-        // } catch(Exception $e) {
-        //     dd($e->getMessage());
-        // }
 
     }
 
@@ -101,16 +120,14 @@ class CompanyAPIController extends BaseController
     {
         //
         try {
-            if(Company::whereId($id)->exists()) {
+            if (Company::whereId($id)->exists()) {
                 $company_deleted_status = Company::deleteCompany($id);
                 return ResponseHandler::success(['Company deleted successfully']);
             } else {
                 return ResponseHandler::validationError(['Company not found']);
             }
-            
         } catch (\Exception $e) {
-            return ResponseHandler::serverError( $e );
+            return ResponseHandler::serverError($e);
         }
-
     }
 }
